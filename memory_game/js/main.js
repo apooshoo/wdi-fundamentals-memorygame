dca6dd43b6e259eb452e505ca0120c2647d08106
Jonathan <rhymenoceroses@gmail.com>
2019-06-15 21:57:00 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -30,17 +30,34 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 }
 }
 
-function flipCard(cardId) {
+
+
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank + ".")
 	cardsInPlay.push(cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
+    this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length < 2) {
 		return;
 	}
 	checkForMatch();
-
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+    for (var i = 0; i < cards.length; i += 1) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', "images/back.png");
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+createBoard();
+
+//Note: I put the checkformatch results in consolelog to prevent popups
+
+// 1) How is the function linked to the array?
+// 2) is it necessary for createboard() to be after flipcard() because flipcard() is included later? what about 'data-id' going the other way?
